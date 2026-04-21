daeeab01f9ecff6dac749ca5325dc25f16577523
J-Faustus <hermanbrendan24@gmail.com>
2026-04-20 21:15:39 -0600

    reformatted Main

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ use crate::FileIO::get_data;
 #[cfg(test)]
 mod test;
 
-mod math;
-mod UI;
 mod FileIO;
+mod UI;
+mod math;
 
-fn main() {    
+fn main() {
     //read file
-    let (prices, names)=match get_data(){
-        Result::Ok((prices, names))=>(prices, names),
-        _=>panic!(),
+    let (prices, names) = match get_data() {
+        Result::Ok((prices, names)) => (prices, names),
+        _ => panic!("unable to get prices"),
     };
     //we want this data to never be overwritten, so we use immutable variables
 
-    let params=UI::Parameters::New(prices, names);
-    let mut state=UI::State{
-        cart:Vec::new(),balance:0,hypothetical:0, checking:UI::CheckLevel::Optimal};
+    let params = UI::Parameters::New(prices, names);
+    let mut state = UI::State {
+        cart: Vec::new(),
+        balance: 0,
+        hypothetical: 0,
+        checking: UI::CheckLevel::Optimal,
+    };
     UI::do_interactive(&mut state, &params);
 }
